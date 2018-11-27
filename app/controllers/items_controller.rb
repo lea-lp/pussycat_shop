49bcee56370194d9f3f169a0876937445968237b
lea-lp <lea.leipp@gmail.com>
2018-11-27 03:14:13 +0100

    end show, begin registration

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,19 +6,12 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @items = Item.all
+
     @sug_items = []
     4.times do 
     random_cat = Item.find(rand(@items.first.id..@items.last.id))
     @sug_items << random_cat
     end 
-    p "*" * 100
-    puts @sug_items
-    p "*" * 100
-
-
-
-
-
   end
 
   def edit
