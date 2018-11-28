d4964051e0e9930da08e5d788b835ff3e1905902
lea-lp <lea.leipp@gmail.com>
2018-11-28 15:16:34 +0100

    mailer ALMOST done...

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,15 +1,13 @@
 class ContactMailer < ApplicationMailer
 
-  def info_order(order)
+  def info_order(admin_email, order)
     @order = order
-    mail(to: 'kasskq@gmail.com', subject:'une commande a été passée!')
-    mail(to: 'clemoun@yahoo.fr', subject:'une commande a été passée!')
+    mail(to: admin_email, subject:'une commande a été passée!')
   end
 
   def order_confirmation(order)
     @order = order
-    mail(to: @order.user.email, subject:'Confirmation de votre commande n° #{@order.id}')
+    mail(to: @order.user.email, subject:"Confirmation de votre commande n° #{@order.id}" )
   end
-
 
 end
