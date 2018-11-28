c0a062c1c7ec94aab212a181f0bc7f13d574b7ab
Thierry <kasskq@gmail.com>
2018-11-28 18:07:12 +0100

    test active storage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,9 @@
 class Item < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :image_url, uniqueness: true
   validates :description, length: { in: 0..500 }
   validates :price, numericality: true
   validates :price, numericality: { greater_than: 0}
   has_and_belongs_to_many :carts
   has_and_belongs_to_many :orders
-
+  has_one_attached :image
 end
